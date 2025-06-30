7af7d63fe23110a81cb3af0bb8a63dfd32c94973
RobloxIce-coder <foltyzxzhen@gmail.com>
2025-06-30 14:29:31 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-document.getElementById("judul").style.color = "blue";
+function peringati() {
+  alert("Hahahhaha, tempat ini telah diserang oleh hantu anomali berkepala tiga!");
+  document.getElementById("utama").style.backgroundColor = "red";
+  let sound = document.createElement("audio");
+  sound.src = "https://audio.jukehost.co.uk/58fx8oCMdBtlBHoAdJzkMpEokQKKtWkW";
+  sound.type = "audio/mpeg"
+  sound.loop = true;
+  sound.play();
+  document.getElementById("gambar").classList.add("geser");
+  document.getElementById("teks-berjalan").classList.add("ganti");
+  let gambar = document.getElementById("gambar");
+  let index = 0;
+  const gambarArray = ["TungTung.jpg","Balerina.jpg","Dontol.jpg","GedaGedi.jpg", "Hiu.jpg","Buaya.jpg", "Gajah.jpg","Monyet.jpg","Garam.jpg", "Patapim.jpg","Bis.jpg","BisHitam.jpg","Kadal.jpg","Pisang.jpg","Assasino.jpg"];
+  setTimeout(() => {
+    gambar.style.animationPlayState = "running";
+    setInterval(() => {
+      gambar.src = gambarArray[index];
+      index = (index + 1) % gambarArray.length;
+    }, 5000); 
+  }, 100);
+}
